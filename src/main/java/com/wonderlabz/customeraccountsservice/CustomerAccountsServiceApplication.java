a55e7995d8c3f2c9697573c8d5e1e4c76466d93c
kudakwashe <kuda@nightsbridge.co.za>
2021-04-05 16:35:20 +0200

    Initial commit

---
+++ src/main/java/com/wonderlabz/customeraccountsservice/CustomerAccountsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wonderlabz.customeraccountsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAccountsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerAccountsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wonderlabz/customeraccountsservice/CustomerAccountsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonderlabz.customeraccountsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAccountsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
